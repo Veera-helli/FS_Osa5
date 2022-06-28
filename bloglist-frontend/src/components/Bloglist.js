11dc0e96788304424f688da4611518c6ebe70cf9
Veera Ihalainen <Veera.helli@gmail.com>
2022-06-28 13:55:48 +0300

    done until 5.6

--- bloglist-frontend/src/components/Bloglist.js
+++ bloglist-frontend/src/components/Bloglist.js
@@ -1,6 +1,7 @@
 import Notification from './Notification';
 import CreateForm from './CreateForm';
 import Blog from './Blog';
+import { useState } from 'react';
 
 const Bloglist = ({
   errorMessage,
@@ -9,25 +10,42 @@ const Bloglist = ({
   setUser,
   blogs,
   setBlogs,
-}) => (
-  <div>
-    <h2>Blogs</h2>
-    <Notification message={errorMessage} />
-    <p>{user.name} logged in</p>
-    <button
-      onClick={() => {
-        window.localStorage.removeItem('loggedBlogappUser');
-        setUser(null);
-      }}
-    >
-      log out
-    </button>
-    <CreateForm setBlogs={setBlogs} setMessage={setMessage} />
+}) => {
+  const [visible, setVisible] = useState(false);
 
-    {blogs.map((blog) => (
-      <Blog key={blog.id} blog={blog} />
-    ))}
-  </div>
-);
+  const hideWhenVisible = { display: visible ? 'none' : '' };
+  const showWhenVisible = { display: visible ? '' : 'none' };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+  return (
+    <div>
+      <h2>Blogs</h2>
+      <Notification message={errorMessage} />
+      <p>{user.name} logged in</p>
+      <button
+        onClick={() => {
+          window.localStorage.removeItem('loggedBlogappUser');
+          setUser(null);
+        }}
+      >
+        log out
+      </button>
+      <div>
+        <div style={hideWhenVisible}>
+          <button onClick={toggleVisibility}>new blog</button>
+        </div>
+        <div style={showWhenVisible}>
+          <CreateForm setBlogs={setBlogs} setMessage={setMessage} />
+          <button onClick={toggleVisibility}>cancel</button>
+        </div>
+      </div>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
 
 export default Bloglist;
